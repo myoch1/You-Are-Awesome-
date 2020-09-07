6248f58d12c1718466803733263259a90b71f05f
myoch1 <myoch@zagmail.gonzaga.edu>
2020-09-07 14:43:36 -0700

    Initial Commit

---
+++ You Are Awesome!!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome!!
+//
+//  Created by Milagros Yoch on 9/7/20.
+//  Copyright © 2020 Milagros Yoch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
