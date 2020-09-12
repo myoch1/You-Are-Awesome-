5c603cb3cc23d67eb7deda91545f04caf29fd264
myoch1 <myoch@zagmail.gonzaga.edu>
2020-09-12 14:30:52 -0700

    2.13, While and Repeat

--- You Are Awesome!!/ViewController.swift
+++ You Are Awesome!!/ViewController.swift
@@ -28,37 +28,17 @@ class ViewController: UIViewController {
                        "Fabulous? That's You!",
                        "You've Got the Design Skills of Jony Ive"]
         
-        messageLabel.text = messages[Int.random(in: 0...messages.count-1)]
-        imageView.image = UIImage(named: "image\(Int.random(in: 0...totalNumberOfImages))")
-    // messageLabel.text = messages[messageNumber]
-    //  messageNumber += 1
-    //    if messageNumber == messages.count {
-    //   messageNumber = 0
+        var newMessage = messages[Int.random(in: 0...messages.count-1)]
+        while messageLabel.text ==  newMessage {
+            print("*** We had a repeating value. Both newMessage and messageLabel.text = \(newMessage)and \(messageLabel.text!)")
+            newMessage = messages[Int.random(in: 0...messages.count-1)]
         }
+        messageLabel.text = newMessage
+    
         
-        // let imageName = "image" + String (imageNumber)
-//        let imageName = "image\(imageNumber)"
-//        imageView.image = UIImage(named: imageName)
-//        imageNumber = imageNumber + 1
-//        if imageNumber == 10 {
-//            imageNumber = 0
-//        }
-        
-        
-                //        let awesomeMessage = "You Are Awesome!"
-                //        let greatMessage = "You Are Great!"
-                //        let bombMessage = "You Are Da Bomb!"
-                //
-                //        if messageLabel.text == awesomeMessage {
-                //            messageLabel.text = greatMessage
-                //            imageView.image = UIImage(named: "image1")
-                //        } else if messageLabel.text == greatMessage {
-                //            messageLabel.text = bombMessage
-                //            imageView.image = UIImage(named: "image2")
-                //        } else {
-                //            messageLabel.text = awesomeMessage
-                //            imageView.image = UIImage(named: "image0")
-                //        }
+        imageView.image = UIImage(named: "image\(Int.random(in: 0...totalNumberOfImages))")
+   
+        }
     }
     
 
