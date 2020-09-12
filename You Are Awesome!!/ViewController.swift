3e4cdd11e2452bfc051b0fd9a6d3a30694efcd4c
myoch1 <myoch@zagmail.gonzaga.edu>
2020-09-12 14:51:02 -0700

    2.13 While/Repeat

--- You Are Awesome!!/ViewController.swift
+++ You Are Awesome!!/ViewController.swift
@@ -12,8 +12,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     
-    var imageNumber = 0
-    var messageNumber = 0
+    var imageNumber = -1
+    var messageNumber = -1
     let totalNumberOfImages = 9
     
     override func viewDidLoad() {
@@ -28,15 +28,20 @@ class ViewController: UIViewController {
                        "Fabulous? That's You!",
                        "You've Got the Design Skills of Jony Ive"]
         
-        var newMessage = messages[Int.random(in: 0...messages.count-1)]
-        while messageLabel.text ==  newMessage {
-            print("*** We had a repeating value. Both newMessage and messageLabel.text = \(newMessage)and \(messageLabel.text!)")
-            newMessage = messages[Int.random(in: 0...messages.count-1)]
-        }
-        messageLabel.text = newMessage
+        var newMessageNumber: Int
+        repeat {
+            newMessageNumber = Int.random(in: 0...messages.count-1)
+        } while messageNumber ==  newMessageNumber
+        messageNumber = newMessageNumber
+        messageLabel.text = messages[messageNumber]
     
         
-        imageView.image = UIImage(named: "image\(Int.random(in: 0...totalNumberOfImages))")
+        var newImageNumber: Int
+        repeat {
+            newImageNumber = Int.random(in: 0...totalNumberOfImages)
+        } while imageNumber == newImageNumber
+        imageNumber = newImageNumber
+        imageView.image = UIImage(named: "image\(imageNumber)")
    
         }
     }
